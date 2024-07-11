3a3e9026c0c855498e46c4f3cd39f60912f303cf
Smirkkkk <zhouwentao@sjtu.edu.cn>
2024-07-11 09:59:21 +0800

    lab3

---
+++ lab3/app/src/main/java/cn/edu/sjtu/zhouwentao/kotlinJpCChatter/Chatt.kt
@@ -0,0 +1,5 @@
+package cn.edu.sjtu.zhouwentao.kotlinJpCChatter
+
+class Chatt(var username: String? = null,
+            var message: String? = null,
+            var timestamp: String? = null)
---
+++ lab3/app/src/main/java/cn/edu/sjtu/zhouwentao/kotlinJpCChatter/MainActivity.kt
@@ -0,0 +1,48 @@
+package cn.edu.sjtu.zhouwentao.kotlinJpCChatter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import cn.edu.sjtu.zhouwentao.kotlinJpCChatter.ui.theme.KotlinJpCChatterTheme
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val navController = rememberNavController()
+            NavHost(navController, startDestination = "MainView") {
+                composable("MainView") {
+                    MainView(this@MainActivity, navController)
+                }
+                composable("PostView") {
+                    PostView(this@MainActivity, navController)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KotlinJpCChatterTheme {
+        Greeting("Android")
+    }
+}
